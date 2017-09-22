f81bf0366dc3f201b29ebdf914808b66f0f3fc39
Domer79 <garipovd@mail.ru>
2017-09-22 16:24:44 +0500

    Добавил Security.Manager

---
+++ Security.Manager/Controllers/SettingsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Security.Model;
+using System.Net;
+
+namespace Security.Manager.Controllers
+{
+    public class SettingsController : BaseSecurityController
+    {        
+        public ActionResult GetSystemSettings()
+        {
+            var settings = CoreSecurity.Settings.GetSystemSettings();
+            return JsonByNewtonsoft(settings, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult SaveSetting(Setting setting)
+        {
+            CoreSecurity.Settings.SetValue<string>(setting.Name, setting.Value);
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+    }
+}
