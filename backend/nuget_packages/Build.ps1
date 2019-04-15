a681a967e69440d27c768ac275c77600ceda1039
Domer79 <garipovd@mail.ru>
2019-04-15 16:12:03 +0500

    Создание пакетов NuGet

--- backend/nuget_packages/Build.ps1
+++ backend/nuget_packages/Build.ps1
@@ -2,6 +2,12 @@
 $buildPath = Get-VSSetupInstance | Where {$_.DisplayName -eq "Visual Studio Community 2017"} | select InstallationPath
 $buildPath = $buildPath.InstallationPath + "\MSBuild\15.0\bin\MSBuild.exe"
 
-& $buildPath 'c:\Users\Domer\source\repos\Security.V2.1\backend\Security\Security.csproj' '/p:TargetFrameworkVersion=v4.6;Configuration=Release;OutDir=..\nuget_packages\Security' '/tv:15.0'
-& $buildPath 'c:\Users\Domer\source\repos\Security.V2.1\backend\SecurityHttp\SecurityHttp.csproj' '/p:TargetFrameworkVersion=v4.6;Configuration=Release;OutDir=..\nuget_packages\SecurityHttp' '/tv:15.0'
-& $buildPath 'c:\Users\Domer\source\repos\Security.V2.1\backend\Security.Web\Security.Web.csproj' '/p:TargetFrameworkVersion=v4.6;Configuration=Release;OutDir=..\nuget_packages\Security.Web' '/tv:15.0'
+#build projects
+& $buildPath 'c:\Users\Domer\source\repos\Security.V2.1\backend\Security\Security.csproj' '/p:TargetFrameworkVersion=v4.6;Configuration=Release;OutDir=..\nuget_packages\lib' '/tv:15.0'
+& $buildPath 'c:\Users\Domer\source\repos\Security.V2.1\backend\SecurityHttp\SecurityHttp.csproj' '/p:TargetFrameworkVersion=v4.6;Configuration=Release;OutDir=..\nuget_packages\lib' '/tv:15.0'
+& $buildPath 'c:\Users\Domer\source\repos\Security.V2.1\backend\Security.Web\Security.Web.csproj' '/p:TargetFrameworkVersion=v4.6;Configuration=Release;OutDir=..\nuget_packages\lib' '/tv:15.0'
+
+#nuget packaging
+./nuget.exe pack Security.nuspec
+./nuget.exe pack SecurityHttp.nuspec
+./nuget.exe pack Security.Web.nuspec
